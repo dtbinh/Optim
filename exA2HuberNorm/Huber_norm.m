a24a9357496a0f4db523969ef4ba7489ac3b67cf
Rian Beck <r0585849@student.kuleuven.be>
2018-11-25 11:26:34 +0100

    added 3 exercises

---
+++ exA2HuberNorm/Huber_norm.m
@@ -0,0 +1,108 @@
+clear all
+close all
+
+addpath('C:\Users\Wolf/casadi-windows-matlabR2016a-v3.4.5')
+import casadi.*
+
+%% Data
+data = [1 2-sqrt(2)/2 2 3 2;
+        0 sqrt(2)/2   1 0 -1];
+data_perturbed = [1 2-sqrt(2)/2 2.5 3 2;
+                  0 sqrt(2)/2   1   0 -1];
+% 
+% x_data = data(1,:);
+% y_data = data(2,:);
+
+x_data = data_perturbed(1,:);
+y_data = data_perturbed(2,:);
+
+radius = 1;
+%% Exercise A.1
+% cost: e1² + e2² + e3² + e4² + e5² = ||e||² where e = [e1 e2 e3 e4 e5]^T
+opti = casadi.Opti();
+%% Huber norm
+si = opti.variable(1,5);
+p = opti.variable(1,2);
+opti.minimize(sum(si));
+opti.set_initial(p, [1.5;0.5])
+
+% x_c = opti.variable(1,1);
+% y_c = opti.variable(1,1);
+cost = 0;
+delta = 0.01;
+x = SX.sym('error');
+% func = piecewise(x<-delta, delta*(-x-0.5*delta), -delta<=x<=delta, 0.5*x^2, x>delta, delta*(x-0.5*delta));
+func = (x<-delta)*(delta*(-x-0.5*delta)) + (-delta<=x)*(x<=delta)*(0.5*x^2) + (x>=delta)*(delta*(x-0.5*delta));
+huber_cost = Function('huber_cost',{x},{func});
+
+for i = 1:length(x_data)
+error = sqrt((x_data(i) - p(1))^2 + (y_data(i) - p(2))^2) - 1;
+cost = cost + huber_cost(error);
+opti.subject_to(-si(i)<=huber_cost(error))
+opti.subject_to(huber_cost(error)<=si(i))
+end
+
+opti.solver('ipopt');
+sol = opti.solve();
+sol.value(p)
+
+alpha = 0:pi/100:2*pi;
+p_hub = sol.value(p);
+x_c_hub = p_hub(1);
+y_c_hub = p_hub(2);
+x_circle_hub = x_c_hub + radius * cos(alpha);
+y_circle_hub = y_c_hub + radius * sin(alpha);
+
+%% L2 norm
+% coordinate of center is optimization variable.
+x_c = opti.variable(1,1);
+y_c = opti.variable(1,1);
+% radius is 1. Could also be taken as variable to optimize.
+cost = sum((sqrt((x_data - x_c).^2 + (y_data - y_c).^2) - radius).^2);
+
+opti.minimize(cost);
+opti.solver('ipopt');
+sol = opti.solve();
+
+% Plot
+alpha = 0:pi/100:2*pi;
+x_c_L2 = sol.value(x_c);
+y_c_L2 = sol.value(y_c);
+x_circle_L2 = x_c_L2 + radius * cos(alpha);
+y_circle_L2 = y_c_L2 + radius * sin(alpha);
+
+%% L1 norm
+
+si = opti.variable(1,5);
+p = opti.variable(1,2);
+opti.minimize(sum(si));
+opti.subject_to(-si<=sqrt((x_data - p(1)).^2 + (y_data - p(2)).^2) - 1)
+opti.subject_to(sqrt((x_data - p(1)).^2 + (y_data - p(2)).^2) -1<=si)
+opti.set_initial(p, [1.5;0.5])
+opti.solver('ipopt');
+sol = opti.solve();
+sol.value(p);
+sol.value(si);
+
+alpha = 0:pi/100:2*pi;
+p_L1 = sol.value(p);
+x_c_L1 = p_L1(1);
+y_c_L1 = p_L1(2);
+x_circle_L1 = x_c_L1 + radius * cos(alpha);
+y_circle_L1 = y_c_L1 + radius * sin(alpha);
+
+%% Plot circles to compare methods
+figure('Name', 'CSomparison between L1 norm, L2 norm and Huber norm')
+hold on
+plot(x_circle_L1, y_circle_L1)
+plot(x_circle_L2, y_circle_L2)
+plot(x_circle_hub, y_circle_hub)
+plot(x_data, y_data)
+legend('L1 norm', 'L2 norm', 'Huber norm', 'data points')
+xlim([0.0 4])
+ylim([-1.5 1.5])
+
+p_L1 
+P_L2 = [x_c_L2, y_c_L2]
+p_hub
+
---
+++ exB3BycicleModel/Bycicle_model.m
@@ -0,0 +1,120 @@
+% Car A-->B multiple shooting
+
+clear all
+close all
+clc
+addpath('C:\Users\Wolf/casadi-windows-matlabR2016a-v3.4.5')
+import casadi.*
+%%
+N = 100; % Control discretization
+m = 80; % vehicle mass [kg]
+L = 1.5;
+
+% Create optimization environment
+opti = casadi.Opti();
+
+% declare variables
+% states
+x = opti.variable(N+1,1); % position
+y = opti.variable(N+1,1);
+theta = opti.variable(N+1,1); % angle
+v = opti.variable(N+1,1); % velocity
+% controls
+a = opti.variable(N,1); % acceleration
+delta = opti.variable(N,1);
+
+T = opti.variable(1); % motion time
+
+% ODE rhs function
+ode = @(x,u)[x(4).*cos(x(3)); x(4).*sin(x(3)); x(4)/L*tan(u(2)) ; u(1)];  % =[xdot, ydot, thetadot, vdot]
+
+% input constraints
+F_min = -100.;
+F_max = 100.;
+delta_min = -pi/4;
+delta_max = pi/4;
+
+% Path constraints
+v_min = -5.;
+v_max = 5.;
+
+% Initial and terminal constraints
+x_init = [0., 0., 0., 0.];
+x_final = [10., 10., 0., 0.];
+
+% Construct all constraints
+
+for k=1:N
+   xk      = [x(k); y(k); theta(k); v(k)];
+   xk_plus = [x(k+1); y(k+1); theta(k+1); v(k+1)];
+   
+   % shooting constraint
+   xf = rk4(ode,T/N,xk,[a(k), delta(k)]);
+   opti.subject_to(xk_plus==xf);
+end
+
+% path constraint
+opti.subject_to(v_min <= v <= v_max);
+opti.subject_to(F_min <= m*a <= F_max);
+opti.subject_to(delta_min <= delta <= delta_max);
+opti.subject_to(T >= 0);
+
+opti.subject_to({x(1)==x_init(1), x(end)==x_final(1), y(1)==x_init(2), y(end)==x_final(2)});
+opti.subject_to({theta(1)==x_init(3), theta(end)==x_final(3), v(1)==x_init(4), v(end)==x_final(4)}); 
+
+% set initial guess
+opti.set_initial(T, 10); % seconds
+
+% Objective function
+opti.minimize(T);
+
+opti.solver('ipopt')
+% solve optimization problem
+sol = opti.solve();
+
+% retrieve the solution
+posx_opt = sol.value(x);
+posy_opt = sol.value(y);
+theta_opt = sol.value(theta);
+vel_opt = sol.value(v);
+a_opt = sol.value(a);
+delta_opt = sol.value(delta);
+T_opt = sol.value(T);
+
+% time grid for printing
+tgrid = linspace(0,T_opt, N+1);
+
+%%
+figure;
+title('State trajectories')
+subplot(3,1,1)
+hold on
+plot(tgrid, posx_opt, 'b-x')
+plot(tgrid, posy_opt, 'r-o')
+xlabel('time [s]')
+ylabel('position [m]')
+
+subplot(3,1,2)
+plot(tgrid, vel_opt, 'b-x')
+xlabel('time [s]')
+ylabel('velocity [m/s]')
+
+subplot(3,1,3)
+stairs(tgrid(1:end-1), m*a_opt, 'b-x')
+xlabel('time [s]')
+ylabel('acceleration [m/s^2]')
+
+figure
+stairs(tgrid(1:end-1), delta_opt, 'b-x')
+xlabel('time [s]')
+ylabel('delta')
+
+figure
+hold on
+plot(posx_opt,posy_opt)
+xlabel('position-x [m]')
+ylabel('position-y [m]')
+title('Top view')
+axis equal
+
+disp(strcat('Optimal motion time: ' , num2str(sol.value(T)), ' s'));
